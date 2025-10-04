49ebedc29de35ccd56315460133596ff261d5b28
ksk02 <keisukehara02@gmail.com>
2025-10-04 20:27:58 +0900

    compojure

--- src/clojure_app/core.clj
+++ src/clojure_app/core.clj
@@ -1,63 +1,19 @@
 (ns clojure-app.core
-  (:require [ring.adapter.jetty :as server]))
+  (:require [compojure.core :refer [routes]]
+            [ring.adapter.jetty :as server]
+            [clojure-app.handler.main :refer [main-routes]]
+            [clojure-app.handler.todo :refer [todo-routes]]))
 
 (defonce server (atom nil))
 
-(defn ok [body]
-  {:status 200
-   :body body})
-
-(defn html [res]
-  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
-
-(defn not-found []
-  {:status 404
-   :body "<h1>404 page not found</h1>"})
-
-(defn home-view [req]
-  "<h1>Welcome to the Home Page</h1>
-   <a href=\"/todo \">Go to Todo List</a>")
-
-(defn home [req]
-  (-> (home-view req)
-      ok
-      html))
-
-(def todo-list
-  [{:title "朝ごはんを作る"}
-   {:title "燃えるゴミを出す"}
-   {:title "卵を買って帰る"}
-   {:title "お風呂を洗う"}])
-
-(defn todo-index-view [req]
-  `("<h1>TODO 一覧</h1>"
-    "<ul>"
-    ~@(for [{:keys [title]} todo-list]
-        (str "<li>" title "</li>"))
-    "</ul>"))
-
-(defn todo-index [req]
-  (-> (todo-index-view req)
-      ok
-      html))
-
-(def routes
-  {"/" home
-   "/todo" todo-index})
-
-(defn match-route [uri]
-  (get routes uri))
-
-(defn handler [req]
-  (let [uri (:uri req)
-        maybe-fn (match-route uri)]
-    (if maybe-fn
-      (maybe-fn req)
-      (not-found))))
+(def app
+  (routes
+   todo-routes
+   main-routes))
 
 (defn start-server []
   (when-not @server
-    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
+    (reset! server (server/run-jetty #'app {:port 3000 :join? false}))))
 
 (defn stop-server []
   (when @server
