e579de30ba97d68c03e1d9280f1a1d9f262697cb
ksk02 <keisukehara02@gmail.com>
2025-11-30 21:15:29 +0900

    駅情報を取得する

--- src/clojure_app/handler/main.clj
+++ src/clojure_app/handler/main.clj
@@ -1,7 +1,9 @@
 (ns clojure-app.handler.main
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.route :as route]
-            [clojure-app.util.response :as res]))
+            [clojure-app.util.response :as res]
+            [clj-http.client :as client]
+            [cheshire.core :as json]))
 
 (defn home-view [req]
   "<h1>ホーム画面</h1>
@@ -10,9 +12,14 @@
 (defn centroid-station-handler [{:keys [params]}]
   (let [stations (:stations params)
         stations (if (vector? stations) stations [stations])
-        result (apply str stations)]
-    (-> (res/response result)
-        (assoc-in [:headers "Content-Type"] "text/plain; charset=utf-8"))))
+        fetch-station (fn [name]
+                        (let [url "https://express.heartrails.com/api/json"
+                              resp (client/get url {:query-params {:method "getStations" :name name}
+                                                    :as :json})]
+                          (:body resp)))
+        result (map fetch-station stations)]
+    (-> (res/response (json/generate-string result))
+        (assoc-in [:headers "Content-Type"] "application/json; charset=utf-8"))))
 
 (defn home [req]
   (-> (home-view req)
